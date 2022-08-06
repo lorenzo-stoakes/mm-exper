60e19d458eb9146cd815b8eb0463c630879e0d78
Lorenzo Stoakes <lstoakes@gmail.com>
2022-08-06 22:54:29 +0100

    exper: extend module test code

--- kernel/exper.c
+++ kernel/exper.c
@@ -5,6 +5,8 @@
 #include <linux/mm_types.h>
 #include <linux/module.h>
 #include <linux/page-flags.h>
+#include <linux/pageblock-flags.h>
+#include <linux/percpu-defs.h>
 
 MODULE_AUTHOR("Lorenzo Stoakes <lstoakes@gmail.com>");
 MODULE_DESCRIPTION("Experiments");
@@ -31,6 +33,16 @@ static int __init exper_init(void)
 	pr_err("kmalloc flags = %lu\n", virt_to_page(ptr)->flags & PAGEFLAGS_MASK);
 
 	kfree(ptr);
+
+	pr_err("HUGETLB_PAGE_ORDER = %u, MAX_ORDER = %u, pageblock_order = %u\n",
+	       HUGETLB_PAGE_ORDER, MAX_ORDER, pageblock_order);
+
+	pr_err("per_cpu_offset(0) = %lx\n", (*(unsigned long *)per_cpu_offset(1)));
+	pr_err("per_cpu_offset(1) = %lx\n", per_cpu_offset(1));
+	pr_err("per_cpu_offset(2) = %lx\n", per_cpu_offset(2));
+	pr_err("processor id =%d\n", smp_processor_id());
+
+	pr_err("%p\n", per_cpu_ptr((unsigned char *)0, 1));
 
 	return 0;
 }
