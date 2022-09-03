709c08103fe5211af361921b4e6a216532a5b09d
Lorenzo Stoakes <lstoakes@gmail.com>
2022-09-03 13:48:58 +0100

    exper: write to allocated pages

--- kernel/exper.c
+++ kernel/exper.c
@@ -15,34 +15,27 @@ MODULE_LICENSE("GPL");
 static int __init exper_init(void)
 {
 	struct page *pg;
-	void *ptr;
+	char *chrs_raw;
+	char *chrs_kmalloc;
 
 	pg = alloc_page(GFP_KERNEL);
 
-	if (PageAnon(pg))
-		pr_err("Anon!\n");
-	else
-		pr_err("Not Anon!\n");
+	chrs_raw = page_to_virt(pg);
+	chrs_raw[0] = 'x';
 
 	pr_err("flags = %lu\n", pg->flags & PAGEFLAGS_MASK);
 
 	__free_page(pg);
 
-	ptr = kmalloc(4096, GFP_KERNEL);
+	chrs_kmalloc = kmalloc(4096, GFP_KERNEL);
+	chrs_kmalloc[0] = 'x';
 
-	pr_err("kmalloc flags = %lu\n", virt_to_page(ptr)->flags & PAGEFLAGS_MASK);
+	pr_err("kmalloc flags = %lu\n", virt_to_page(chrs_kmalloc)->flags & PAGEFLAGS_MASK);
 
-	kfree(ptr);
+	kfree(chrs_kmalloc);
 
 	pr_err("HUGETLB_PAGE_ORDER = %u, MAX_ORDER = %u, pageblock_order = %u\n",
 	       HUGETLB_PAGE_ORDER, MAX_ORDER, pageblock_order);
-
-	pr_err("per_cpu_offset(0) = %lx\n", (*(unsigned long *)per_cpu_offset(1)));
-	pr_err("per_cpu_offset(1) = %lx\n", per_cpu_offset(1));
-	pr_err("per_cpu_offset(2) = %lx\n", per_cpu_offset(2));
-	pr_err("processor id =%d\n", smp_processor_id());
-
-	pr_err("%p\n", per_cpu_ptr((unsigned char *)0, 1));
 
 	return 0;
 }
