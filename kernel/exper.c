6bd544625aaf3ed87b3163d3c6425165d1a4be23
Lorenzo Stoakes <lstoakes@gmail.com>
2022-09-03 19:26:36 +0100

    exper: expose page refcount

--- kernel/exper.c
+++ kernel/exper.c
@@ -1,9 +1,11 @@
 #include <linux/fs.h>
 #include <linux/gfp.h>
 #include <linux/init.h>
+#include <linux/miscdevice.h>
 #include <linux/mm.h>
 #include <linux/mm_types.h>
 #include <linux/module.h>
+#include <linux/page_ref.h>
 #include <linux/page-flags.h>
 #include <linux/pageblock-flags.h>
 #include <linux/percpu-defs.h>
@@ -11,6 +13,56 @@
 MODULE_AUTHOR("Lorenzo Stoakes <lstoakes@gmail.com>");
 MODULE_DESCRIPTION("Experiments");
 MODULE_LICENSE("GPL");
+
+static char refcount_buf[255] = "0";
+static int refcount_buf_size = 2;
+
+static ssize_t refcount_read_(struct file *file, char __user *out,
+			      size_t size, loff_t *off)
+{
+	return simple_read_from_buffer(out, size, off, refcount_buf,
+				       refcount_buf_size);
+}
+
+static ssize_t refcount_write_(struct file *file, const char __user *in,
+			       size_t size, loff_t *off)
+{
+	char pfn_buf[255];
+	uint64_t pfn;
+	ssize_t count;
+	int ref_count;
+	struct page *pg;
+
+	count = simple_write_to_buffer(pfn_buf, 254, off, in, size);
+	if (count <= 0)
+		return -EINVAL;
+	pfn_buf[count] = '\0';
+
+	if (sscanf(pfn_buf, "%llu", &pfn) != 1)
+		return -EINVAL;
+
+	pg = pfn_to_page(pfn);
+	if (pg == NULL)
+		return -EINVAL;
+
+	ref_count = page_ref_count(pg);
+	refcount_buf_size = snprintf(refcount_buf, 255, "%d", ref_count) + 1;
+
+	*off = size;
+	return size;
+}
+
+static const struct file_operations refcount_fops = {
+	.owner = THIS_MODULE,
+	.read = refcount_read_,
+	.write = refcount_write_
+};
+
+static struct miscdevice refcount_dev = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "refcount",
+	.fops = &refcount_fops
+};
 
 static int __init exper_init(void)
 {
@@ -37,11 +89,12 @@ static int __init exper_init(void)
 	pr_err("HUGETLB_PAGE_ORDER = %u, MAX_ORDER = %u, pageblock_order = %u\n",
 	       HUGETLB_PAGE_ORDER, MAX_ORDER, pageblock_order);
 
-	return 0;
+	return misc_register(&refcount_dev);
 }
 
 static void __exit exper_exit(void)
 {
+	misc_deregister(&refcount_dev);
 }
 
 module_init(exper_init);
