5eeface737076b6259e876198f119d0030b3e82b
Lorenzo Stoakes <lstoakes@gmail.com>
2022-11-14 23:05:36 +0000

    memstat: add silent flag

--- memstat/get-remote.c
+++ memstat/get-remote.c
@@ -1,20 +1,37 @@
 #include "memstat.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static void usage(const char *bin)
+{
+	fprintf(stderr, "usage: %s [pid to trace] <-s>\n", bin);
+}
 
 int main(int argc, char **argv)
 {
 	const char *pid;
 	struct memstat **mstats;
 	int i;
+	bool silent = false;
 
 	if (argc < 2) {
-		fprintf(stderr, "usage: %s [pid to trace]\n", argv[0]);
+		usage(argv[0]);
 		return EXIT_FAILURE;
 	}
 
 	pid = argv[1];
+	if (argc >= 3) {
+		if (strncmp(argv[2], "-s", sizeof("-s")) != 0) {
+			usage(argv[0]);
+			return EXIT_FAILURE;
+		}
+
+		silent = true;
+	}
+
 	mstats = memstat_snapshot_all(pid);
 
 	// Should have already reported error.
@@ -26,8 +43,10 @@ int main(int argc, char **argv)
 		if (mstat == NULL)
 			break;
 
-		printf("\n");
-		memstat_print(mstat);
+		if (!silent) {
+			printf("\n");
+			memstat_print(mstat);
+		}
 	}
 
 	memstat_free_all(mstats);
