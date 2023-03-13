164d3099ff47a52036c9a4f2949e8fded61aabaa
Lorenzo Stoakes <lstoakes@gmail.com>
2023-03-13 18:36:27 +0000

    random: add readwritefile

---
+++ random/readwritefile.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main(void)
+{
+	const long page_size = sysconf(_SC_PAGESIZE);
+
+	int fd = open("test.txt", O_RDWR);
+	if (fd == -1) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+
+	// Map shared the file.
+	char *ptr = mmap(NULL, page_size, PROT_READ | PROT_WRITE,
+			 MAP_SHARED, fd, 0);
+	if (ptr == MAP_FAILED) {
+		perror("mmap");
+		return EXIT_FAILURE;
+	}
+
+	// _Read_ fault this in.
+	char buf[3];
+	buf[0] = ptr[0];
+
+	// Now madvise MADV_NORMAL to make it easy to put a breakpoint in gdb
+	// (at madvise_walk_vmas()).
+	madvise(ptr, page_size, MADV_NORMAL);
+
+	// _Write_ fault this in.
+	ptr[0] = 'x';
+
+	madvise(ptr, page_size, MADV_NORMAL);
+
+	return EXIT_SUCCESS;
+}
