b3f71f71dfc4588b1d2c01f372ea9fb3d031f4ba
Lorenzo Stoakes <lstoakes@gmail.com>
2023-03-17 15:47:50 +0000

    concepts: add initial raw fault_around concept

---
+++ concepts/fault_around/fault_around.cc
@@ -0,0 +1,227 @@
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <pthread.h>
+#include <sched.h>
+#include <thread>
+
+#include "shared.h"
+
+#define CHECK
+
+#define MAX_NUM_CPUS (256)
+
+#define ALIGN_DOWN(n, to) (n & ~(to - 1ul))
+
+// Reduced version
+struct vm_area_struct {
+	unsigned long vm_start;
+	unsigned long vm_end;
+	unsigned long vm_pgoff;
+};
+
+struct vm_fault {
+	unsigned long address;
+	struct vm_area_struct vma;
+	pgoff_t pgoff;
+};
+
+static vm_fault fault_states[MAX_NUM_CPUS] = {};
+static unsigned int seeds[MAX_NUM_CPUS] = {};
+static std::thread threads[MAX_NUM_CPUS] = {};
+
+std::mutex io_mutex;
+
+static inline unsigned long vma_pages(struct vm_area_struct *vma)
+{
+	return (vma->vm_end - vma->vm_start) >> PAGE_SHIFT;
+}
+
+template<typename T>
+void under_io_lock(T&& fn)
+{
+	std::lock_guard guard(io_mutex);
+
+	fn();
+}
+
+// The kernel version of do_fault_around().
+std::pair<unsigned long, unsigned long> original_fault_around(struct vm_fault *vmf, unsigned long fault_around_bytes)
+{
+	unsigned long address = vmf->address, nr_pages, mask;
+	pgoff_t start_pgoff = vmf->pgoff;
+	pgoff_t end_pgoff;
+	int off;
+
+	nr_pages = READ_ONCE(fault_around_bytes) >> PAGE_SHIFT;
+	mask = ~(nr_pages * PAGE_SIZE - 1) & PAGE_MASK;
+
+	address = max(address & mask, vmf->vma.vm_start);
+	off = ((vmf->address - address) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1);
+	start_pgoff -= off;
+
+	/*
+	 *
+	 *  end_pgoff is either the end of the page table, the end of
+	 *  the vma or nr_pages from start_pgoff, depending what is nearest.
+	 */
+	end_pgoff = start_pgoff -
+		((address >> PAGE_SHIFT) & (PTRS_PER_PTE - 1)) +
+		PTRS_PER_PTE - 1;
+	end_pgoff = min3(end_pgoff, vma_pages(&vmf->vma) + vmf->vma.vm_pgoff - 1,
+			start_pgoff + nr_pages - 1);
+
+	return { start_pgoff, end_pgoff };
+}
+
+std::pair<unsigned long, unsigned long> do_fault_around(struct vm_fault *vmf, unsigned long fault_around_bytes)
+{
+	/*
+	 *  end_pgoff is either the end of the page table, the end of
+	 *  the vma or nr_pages from start_pgoff, depending what is nearest.
+	 */
+	unsigned long fault_pages = READ_ONCE(fault_around_bytes) >> PAGE_SHIFT;
+	int pte_offset = pte_index(vmf->address);
+	int pte_from = ALIGN_DOWN(pte_offset, fault_pages);
+	int pte_to = min(pte_from + fault_pages - 1, PTRS_PER_PTE - 1);
+	pgoff_t start_pgoff = max(vmf->pgoff + pte_from - pte_offset, vmf->vma.vm_pgoff);
+	pgoff_t end_pgoff = min(vmf->pgoff + pte_to - pte_offset,
+				vmf->vma.vm_pgoff + vma_pages(&vmf->vma));
+
+	return { start_pgoff, end_pgoff };
+}
+
+// Get random number between [from, to) * mult and aligned to align.
+uint64_t get_random(unsigned cpu, uint64_t from, uint64_t to, uint64_t align = 1, uint64_t mult = 1)
+{
+	uint64_t delta = rand_r(&seeds[cpu]) % (to - from);
+
+	return ALIGN_DOWN(mult * (from + delta), align);
+}
+
+vm_fault& gen_vmf(unsigned cpu)
+{
+	auto& vmf = fault_states[cpu];
+	auto& vma = vmf.vma;
+
+	vma.vm_start = get_random(cpu, 0x700000000UL, 0x800000000UL, 1, PAGE_SIZE);
+	vma.vm_end = vma.vm_start + get_random(cpu, 1, 100, 1, PAGE_SIZE);
+	vmf.address = get_random(cpu, vma.vm_start, vma.vm_end, PAGE_SIZE);
+	vmf.vma.vm_pgoff = get_random(cpu, 0, 100);
+	vmf.pgoff = vmf.vma.vm_pgoff + ((vmf.address - vma.vm_start) >> PAGE_SHIFT);
+
+#ifdef CHECK
+	if (vmf.vma.vm_start >= vmf.vma.vm_end) {
+		under_io_lock([&] {
+			std::cout << std::hex << "vm_start=" << vmf.vma.vm_start << ">= ";
+			std::cout << "vm_end=" << vmf.vma.vm_end << "\n";
+			exit(EXIT_FAILURE);
+		});
+	}
+
+	if (vmf.address < vmf.vma.vm_start) {
+		under_io_lock([&] {
+			std::cout << std::hex << "vm_start=" << vmf.vma.vm_start << "> ";
+			std::cout << "address=" << vmf.address << "\n";
+			exit(EXIT_FAILURE);
+		});
+	}
+
+	if (vmf.address >= vmf.vma.vm_end) {
+		under_io_lock([&] {
+			std::cout << std::hex << "vm_end=" << vmf.vma.vm_end << "<= ";
+			std::cout << "address=" << vmf.address << "\n";
+			exit(EXIT_FAILURE);
+		});
+	}
+
+	const auto address_offset = (vmf.address - vmf.vma.vm_start) >> PAGE_SHIFT;
+	if (vmf.pgoff != address_offset + vmf.vma.vm_pgoff) {
+		under_io_lock([&] {
+			std::cout << std::hex << "vm_pgoff=" << vmf.vma.vm_pgoff << ", ";
+			std::cout << "address=" << vmf.address << ", offset=" <<  address_offset;
+			std::cout << ", pgoff=" << vmf.pgoff << ": invalid!\n";
+			exit(EXIT_FAILURE);
+		});
+	}
+#endif
+
+	return vmf;
+}
+
+void do_test(unsigned cpu)
+{
+	vm_fault& vmf = gen_vmf(cpu);
+	const unsigned long fault_around_bytes =
+		rounddown_pow_of_two(get_random(cpu, PAGE_SIZE, PTRS_PER_PTE * PAGE_SIZE));
+
+	const auto [ orig_start_pgoff, orig_end_pgoff ] =
+		original_fault_around(&vmf, fault_around_bytes);
+
+	const auto [ start_pgoff, end_pgoff ] =
+		     do_fault_around(&vmf, fault_around_bytes);
+
+	if (start_pgoff != orig_start_pgoff || end_pgoff != orig_end_pgoff) {
+		under_io_lock([&] {
+			std::cout << std::hex << "vm_start=" << vmf.vma.vm_start << ", vm_end=" << vmf.vma.vm_end;
+			std::cout << std::dec << ", vm_pgoff=" << vmf.vma.vm_pgoff << ", address=" << std::hex << vmf.address;
+			std::cout << std::dec << ", pgoff=" << vmf.pgoff << ", fault_around_bytes=" << fault_around_bytes << "\n";
+
+			std::cout << "MISMATCH: orig = [" << orig_start_pgoff << ", " << orig_end_pgoff << "], ";
+			std::cout << "curr = [" << start_pgoff << ", " << end_pgoff << "]\n";
+
+			std::cout.flush();
+			//exit(EXIT_FAILURE);
+		});
+	}
+}
+
+void thread_worker(unsigned cpu)
+{
+	under_io_lock([cpu] {
+		std::cout << cpu << " started\n";
+	});
+
+	while (true) {
+		do_test(cpu);
+	}
+}
+
+
+int main()
+{
+	const unsigned num_cpus = std::thread::hardware_concurrency();
+
+	if (num_cpus > MAX_NUM_CPUS) {
+		std::cerr << "System has " << num_cpus << " which exceeds max of " << MAX_NUM_CPUS << "\n";
+		return EXIT_FAILURE;
+	}
+
+	for (unsigned i = 0; i < num_cpus; i++) {
+		seeds[i] = i;
+
+		threads[i] = std::thread([i] {
+			cpu_set_t cpuset;
+
+			CPU_ZERO(&cpuset);
+			CPU_SET(i, &cpuset);
+
+			if (pthread_setaffinity_np(threads[i].native_handle(),
+						   sizeof(cpu_set_t), &cpuset)) {
+				perror("setaffinity()");
+				exit(EXIT_FAILURE);
+			}
+
+			thread_worker(i);
+		});
+	}
+
+	for (auto& thread : threads) {
+		thread.join();
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ concepts/fault_around/shared.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#define CONFIG_X86_64
+
+#define PAGE_SHIFT (12)
+#define PAGE_SIZE (1UL << PAGE_SHIFT)
+#define PAGE_MASK (~(PAGE_SIZE - 1))
+#define PTRS_PER_PTE (512)
+
+#define pgoff_t unsigned long
+
+#define READ_ONCE(x) (x)
+
+#define max(x, y) ((x) > (y) ? (x) : (y))
+#define min(x, y) ((x) < (y) ? (x) : (y))
+#define min3(x, y, z) min((typeof(x))min(x, y), z)
+
+static inline unsigned long pte_index(unsigned long address)
+{
+	return (address >> PAGE_SHIFT) & (PTRS_PER_PTE - 1);
+}
+
+typedef unsigned u32;
+typedef unsigned long u64;
+typedef unsigned long __u64;
+
+static __always_inline int fls(unsigned int x)
+{
+	int r;
+
+#ifdef CONFIG_X86_64
+	/*
+	 * AMD64 says BSRL won't clobber the dest reg if x==0; Intel64 says the
+	 * dest reg is undefined if x==0, but their CPU architect says its
+	 * value is written to set it to the same as before, except that the
+	 * top 32 bits will be cleared.
+	 *
+	 * We cannot do this on 32 bits because at the very least some
+	 * 486 CPUs did not behave this way.
+	 */
+	asm("bsrl %1,%0"
+	    : "=r" (r)
+	    : "rm" (x), "0" (-1));
+#elif defined(CONFIG_X86_CMOV)
+	asm("bsrl %1,%0\n\t"
+	    "cmovzl %2,%0"
+	    : "=&r" (r) : "rm" (x), "rm" (-1));
+#else
+	asm("bsrl %1,%0\n\t"
+	    "jnz 1f\n\t"
+	    "movl $-1,%0\n"
+	    "1:" : "=r" (r) : "rm" (x));
+#endif
+	return r + 1;
+}
+
+static __always_inline int fls64(__u64 x)
+{
+	int bitpos = -1;
+	/*
+	 * AMD64 says BSRQ won't clobber the dest reg if x==0; Intel64 says the
+	 * dest reg is undefined if x==0, but their CPU architect says its
+	 * value is written to set it to the same as before.
+	 */
+	asm("bsrq %1,%q0"
+	    : "+r" (bitpos)
+	    : "rm" (x));
+	return bitpos + 1;
+}
+
+static __always_inline __attribute__((const))
+int __ilog2_u32(u32 n)
+{
+	return fls(n) - 1;
+}
+
+static __always_inline __attribute__((const))
+int __ilog2_u64(u64 n)
+{
+	return fls64(n) - 1;
+}
+
+#define ilog2(n) \
+( \
+	__builtin_constant_p(n) ?	\
+	((n) < 2 ? 0 :			\
+	 63 - __builtin_clzll(n)) :	\
+	(sizeof(n) <= 4) ?		\
+	__ilog2_u32(n) :		\
+	__ilog2_u64(n)			\
+ )
+
+static inline unsigned fls_long(unsigned long l)
+{
+	if (sizeof(l) == 4)
+		return fls(l);
+	return fls64(l);
+}
+
+static inline __attribute__((const))
+unsigned long __rounddown_pow_of_two(unsigned long n)
+{
+	return 1UL << (fls_long(n) - 1);
+}
+
+#define rounddown_pow_of_two(n)			\
+(						\
+	__builtin_constant_p(n) ? (		\
+		(1UL << ilog2(n))) :		\
+	__rounddown_pow_of_two(n)		\
+ )
