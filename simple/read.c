0c05a0a2c6df9f4848af580802b01df46fd008db
Lorenzo Stoakes <lstoakes@gmail.com>
2023-09-19 07:24:05 +0100

    Revert "simple: read: do not accept read faililng to read bytes"
    This reverts commit d977ae7f328b6b5b27eaf4dc2f269f2ebf841d43.

--- simple/read.c
+++ simple/read.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	int fd = open("test.txt", O_RDONLY);
+	ssize_t num_bytes = 0;
 	int err = EXIT_FAILURE;
 	struct stat statbuf;
 	char *buf;
-	off_t size;
-	ssize_t bytes_;
+	size_t size;
 
 	if (fd == -1) {
 		perror("open");
@@ -24,29 +24,27 @@ int main(void)
 	}
 
 	size = statbuf.st_size;
-
 	if (size == 0) {
 		err = EXIT_SUCCESS;
 		goto exit_close;
 	}
 
 	buf = malloc(size + 1);
-
 	if (!buf) {
 		perror("malloc");
 		goto exit_close;
 	}
 
-	bytes_read = read(fd, buf, size);
+	do {
+		ssize_t ret = read(fd, &buf[num_bytes], size - num_bytes);
 
-	if (bytes_read == -1) {
-		perror("read");
-		goto exit_free;
-	} else if (bytes_read < size) {
-		fprintf(stderr, "Read %ld bytes, expected %ld",
-			bytes_read, size);
-		goto exit_free;
-	}
+		if (ret == -1) {
+			perror("read");
+			goto exit_free;
+		}
+
+		num_bytes += ret;
+	} while (num_bytes < size);
 
 	buf[size] = '\0';
 	printf("%s", buf);
