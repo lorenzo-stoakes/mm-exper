c7eb0c91783e1983bf20a9b0c295b123bd58fdeb
Lorenzo Stoakes <lstoakes@gmail.com>
2023-11-03 12:45:50 +0000

    random: improve memfd_repro

--- random/memfd_repro.c
+++ random/memfd_repro.c
@@ -7,24 +7,31 @@
 
 int main()
 {
-       int fd = memfd_create("test", MFD_ALLOW_SEALING);
-       if (fd == -1) {
-	       perror("memfd_create");
-	       return EXIT_FAILURE;
-       }
+	const long page_size = sysconf(_SC_PAGESIZE);
+	int fd = memfd_create("test", MFD_ALLOW_SEALING);
+	char *buf;
 
-       write(fd, "test", 4);
+	if (fd == -1) {
+		perror("memfd_create");
+		return EXIT_FAILURE;
+	}
 
-       if (fcntl(fd, F_ADD_SEALS, F_SEAL_WRITE) == -1) {
-	       perror("fcntl");
-	       return EXIT_FAILURE;
-       }
+	write(fd, "test", sizeof("test"));
 
-       void *ret = mmap(NULL, 4, PROT_READ, MAP_SHARED, fd, 0);
-       if (ret == MAP_FAILED) {
-	       perror("mmap");
-	       return EXIT_FAILURE;
-       }
+	if (fcntl(fd, F_ADD_SEALS, F_SEAL_WRITE) == -1) {
+		perror("fcntl");
+		return EXIT_FAILURE;
+	}
 
-       return EXIT_SUCCESS;
+	buf = mmap(NULL, page_size, PROT_READ, MAP_SHARED, fd, 0);
+	if (buf == MAP_FAILED) {
+		perror("mmap");
+		return EXIT_FAILURE;
+	}
+
+	close(fd);
+
+	printf("%s\n", buf);
+
+	return EXIT_SUCCESS;
 }
